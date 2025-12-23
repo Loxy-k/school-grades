de1519c282d80a0859ff432cdac1cb071d61cc15
Loxy-k <msukwalombani@gmail.com>
2025-12-23 19:07:41 +0200

    Delete scripts directory

--- scripts/inspect_students.py
+++
@@ -1,9 +0,0 @@
-import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'school_grades.settings')
-import django
-django.setup()
-from grades.models import Student
-
-print('Students:')
-for s in Student.objects.all():
-    print('ID:', s.id, 'student_id:', s.student_id, 'form:', s.get_form_display(), 'level:', s.level, 'user:', getattr(s.user, 'username', None))
